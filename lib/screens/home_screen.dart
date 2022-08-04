06429f9ed12c60e9727cf3e3e9e44cd5683c2d7d
Oleksii <oleksii.sydorenko.dev@gmail.com>
2022-08-04 21:07:59 +0200

    tabs is ready

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/const/vars.dart';
 import 'package:news_app/services/utilse.dart';
 import 'package:news_app/widgets/drawer_widget.dart';
+import 'package:news_app/widgets/tabs.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  var newsType = NewsType.allNews;
   @override
   Widget build(BuildContext context) {
     final Color color = Utils(context).getColor;
@@ -37,7 +40,47 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         drawer: const DrawerWidget(),
-        body: Container(),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  TabsWidget(
+                    text: 'All news',
+                    color: newsType == NewsType.allNews
+                        ? Theme.of(context).cardColor
+                        : Colors.transparent,
+                    function: () {
+                      if (newsType == NewsType.allNews) {
+                        return;
+                      }
+                      setState(() {
+                        newsType = NewsType.allNews;
+                      });
+                    },
+                    fontSize: newsType == NewsType.allNews ? 22 : 14,
+                  ),
+                  TabsWidget(
+                    text: 'Top trending',
+                    color: newsType == NewsType.topTrending
+                        ? Theme.of(context).cardColor
+                        : Colors.transparent,
+                    function: () {
+                      if (newsType == NewsType.topTrending) {
+                        return;
+                      }
+                      setState(() {
+                        newsType = NewsType.topTrending;
+                      });
+                    },
+                    fontSize: newsType == NewsType.topTrending ? 22 : 14,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
